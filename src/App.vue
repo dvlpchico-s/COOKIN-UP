982307ad2590ae57e42105136d7012b30525bb2f
Vitor Francisco <vitorfariasdev@gmail.com>
2025-04-03 16:36:38 -0300

    Ainda em densevolvimento

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Banner from './componentes/Banner.vue';
+import Principal from './componentes/Principal.vue';
+
+export default {
+  components: {Banner,Principal} //Isso daqui é tipo chamar um objeto que está dentro do componentes
+}
+</script>
+<!-- ---------------T---E---M---P--L---A---T---E------------------- -->
+<template>
+  <Banner />
+  <Principal />
+
+</template>
+<!-- --------------C--------S----------S--------------------------- -->
+<style scoped>
+</style>
